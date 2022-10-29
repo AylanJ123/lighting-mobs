dca748a260c1fb286c1c5552dbbb9172b23fd4ed
AylanJ123 <aylanjosue@gmail.com>
2022-10-29 00:50:12 -0600

    Sword fixed

--- src/main/java/com/modcrafting/lightningmobs/items/lightningsword/LightningSword.java
+++ src/main/java/com/modcrafting/lightningmobs/items/lightningsword/LightningSword.java
@@ -5,13 +5,11 @@ import com.modcrafting.lightningmobs.ModTiers;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.SwordItem;
-import net.minecraft.world.item.Tiers;
-
 
 public class LightningSword extends SwordItem {
 
 	public LightningSword(Properties properties) {
-		super(ModTiers.LIGHTNING, 2, 9, properties);
+		super(ModTiers.LIGHTNING, 3, -2f, properties);
 	}
 
 	public static LightningSword init() {
